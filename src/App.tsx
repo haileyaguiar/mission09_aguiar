d6b5763bd773db0e438da2c7624ac45a5b32f0d9
HaileySanders82 <103207122+HaileySanders82@users.noreply.github.com>
2024-03-05 10:21:10 -0700

    app.tsx main changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+    return (
+      <div>
+        <h2>{oneTeam.school}</h2>
+        <h3>{oneTeam.name}</h3>
+        <h3>
+          {oneTeam.city}, {oneTeam.state}
+        </h3>
+        <div />
+      </div>
+    );
+  }
+}
+
+function NewList() {
+  const teams: TeamProps[] = require('./CollegeBasketballTeams.json');
+  return (
+    <div>
+      {teams.map((singleTeam: TeamProps) => (
+        <Team key={singleTeam.tid} {...singleTeam} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <p>Hello, World!</p>
+      <header>College Basketball Teams</header>
+      <NewList />
     </div>
   );
 }
